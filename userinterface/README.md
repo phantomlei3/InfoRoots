1adc80eaf2c10ac161a838a4da53692ed1e3ccc7
Joseph Om <joeom516@gmail.com>
2020-02-08 18:42:41 -0500

    Frontend: Initial setup

---
+++ userinterface/README.md
@@ -0,0 +1,9 @@
+## Description ##
+This repository holds the frontend for InfoRoots. 
+
+## To run the client ##  
+
+```
+$ npm install
+$ npm start
+```
---
+++ userinterface/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="top">
+        <div className="title">
+          InfoRoots
+        </div>
+      </div>
+      <div id="bottom">
+      </div>
+    </div>
+  );
+}
+
+export default App;
