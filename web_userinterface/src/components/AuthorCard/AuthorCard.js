69b34891415b3625f2efd55eacf346bd5c73de85
Joseph Om <joeom516@gmail.com>
2020-03-25 01:45:50 -0400

    Frontend: Add AuthorCard scaffold

---
+++ web_userinterface/src/components/AuthorCard/AuthorCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./AuthorCard.css";
+import Rater from "react-rater";
+import "react-rater/lib/react-rater.css";
+
+class AuthorCard extends React.Component {
+  render() {
+    return (
+      <div className="author_card">
+        <div id="author">
+          {this.props.author}
+          <div id="author-profile-link">
+            <a
+              id="author-link-a"
+              href="https://github.com/phantomlei3/InfoRoots"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              See verified profile
+            </a>
+          </div>
+        </div>
+        <div id="description">{this.props.description}</div>
+        <div id="credibility">
+          credibility
+          <Rater
+            total={5}
+            interactive={false}
+            rating={this.props.credibility}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AuthorCard;
--- web_userinterface/src/components/SearchArticle/SearchArticle.js
+++ web_userinterface/src/components/SearchArticle/SearchArticle.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SearchArticle.css";
 import NavBar from "../NavBar/NavBar.js";
+import AuthorCard from "../AuthorCard/AuthorCard.js";
 
 class SearchArticle extends React.Component {
   constructor(props) {
@@ -10,9 +11,19 @@ class SearchArticle extends React.Component {
       author: "Dr.Marshall",
       publisher: "People's Publishing House",
       articleText:
-        "Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people."
+        "Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.Chongqing, China The Wuhan Red Cross and Hubei provincial Red Cross have come under fire after donations of crucial medical supplies from across China failed to arrive at the hospitals on the front lines of a coronavirus outbreak that has killed more than 300 people.",
+      authorCardToggled: false
     };
   }
+
+  // Function to handle toggling the Author Card
+  toggleAuthorCard = () => {
+    if (this.state.authorCardToggled === false) {
+      this.setState({ authorCardToggled: true });
+    } else {
+      this.setState({ authorCardToggled: false });
+    }
+  };
 
   render() {
     return (
@@ -22,7 +33,18 @@ class SearchArticle extends React.Component {
           <div id="text-container">
             <div id="title-container">{this.state.title}</div>
             <div id="author-container">
-              Author: <span>{this.state.author}</span>
+              Author:
+              <div id="author-text" onClick={this.toggleAuthorCard}>
+                {this.state.author}
+              </div>
+              {this.state.authorCardToggled && (
+                <AuthorCard
+                  author={this.state.author}
+                  description="His interests are in decision making from data in complex systems, including machine learning, computational finance. He enjoys poker bridge squash..."
+                  credibility={4.5}
+                  authorLink="http://google.com"
+                />
+              )}
             </div>
             <div id="publisher-container">
               Publisher: <span>{this.state.publisher}</span>
