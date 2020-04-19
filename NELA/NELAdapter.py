3b4d30ed0e9221eff313a37c0da808937a52fd0e
phantomlei3 <phantomlei3@gmail.com>
2020-04-19 02:36:49 -0400

    update NELApapter with scaling factor

--- NELA/NELAdapter.py
+++ NELA/NELAdapter.py
@@ -8,12 +8,23 @@ NELA toolkits does not have any class, so we will directly use its function to f
 '''
 
 from NELA.credibility_toolkit import parse_text
+import math
 
 
 class NELAdapter:
 
     def __init__(self, article_title, article_content):
-        self.NELA = parse_text(article_title, article_content)
+        # decode title and content
+        decoded_article_title = article_title.encode("gbk", 'ignore').decode("gbk", "ignore")
+        decoded_article_content = article_content.encode("gbk", 'ignore').decode("gbk", "ignore")
+        self.NELA = parse_text(decoded_article_title, decoded_article_content)
+
+
+    def scale_factor(self, value):
+        new_value = value * 2 - 1
+        if new_value < 0:
+            return 0
+        return new_value
 
     def get_reliability_score(self):
         '''
@@ -22,7 +33,7 @@ class NELAdapter:
         '''
         for item in self.NELA:
             if item["name"] == "fake_filter":
-                return item["result"][1][1]
+                return self.scale_factor(item["result"][1][1])
 
     def get_bias_score(self):
         '''
@@ -31,5 +42,5 @@ class NELAdapter:
         '''
         for item in self.NELA:
             if item["name"] == "bias_filter":
-                return item["result"][1][1]
+                return self.scale_factor(item["result"][0][1])
 
